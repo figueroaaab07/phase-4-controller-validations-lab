833ff1a37a25dffc8c6a65c23b564c4dea95efa0
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-25 21:12:33 -0700

    Completed Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,15 @@
 class AuthorsController < ApplicationController
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -16,6 +17,10 @@ class AuthorsController < ApplicationController
   
   def author_params
     params.permit(:email, :name)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
   
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,16 @@
 class PostsController < ApplicationController
+  # added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+    post.update!(post_params)
     render json: post
   end
 
@@ -18,6 +18,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
